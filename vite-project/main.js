0376ae2ff17ebc7a7681aeedb4cb0ceab6df3150
NickSchirripa <108542730+NickSchirripa@users.noreply.github.com>
2024-11-15 18:26:52 -0500

    Refactor, winning card function.
    -Refactored code.
    
    -made a function called winingCard, that figures out which card wins, and logs out a message for the winning card. This will later be used to add points to the winning player.

--- vite-project/main.js
+++ vite-project/main.js
@@ -2,9 +2,45 @@ let deckId = "";
 let cardRender = document.getElementById("cards");
 let cardBorder = document.getElementById("cardBorder");
 let cardBorderTwo = document.getElementById("cardBorderTwo");
+let newDeck = document.getElementById("new-deck");
+let newCards = document.getElementById("draw");
+let card1 = {
+  value: "",
+};
+let card2 = {
+  value: "",
+};
+
+function winningCard(cardOne, cardTwo) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+
+  const cardOneValue = valueOptions.indexOf(cardOne.value);
+  const cardTwoValue = valueOptions.indexOf(cardTwo.value);
+  if (cardOneValue > cardTwoValue) {
+    console.log("card one wins");
+  } else if (cardTwoValue > cardOneValue) {
+    console.log("card two is the winner");
+  } else {
+    console.log("it is a tie");
+  }
+}
 
 function getDeck() {
-  document.getElementById("draw").style.display = "block";
+  newCards.style.display = "block";
   fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
     .then((res) => res.json())
     .then((data) => {
@@ -21,15 +57,17 @@ function drawCards() {
   fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.cards);
       cardBorder.style.display = "none";
       cardBorderTwo.style.display = "none";
       cardRender.innerHTML = `
             <img src=${data.cards[0].image} />
             <img src=${data.cards[1].image} />
         `;
+      card1.value = data.cards[0].value;
+      card2.value = data.cards[1].value;
+      winningCard(card1, card2);
     });
 }
-document.getElementById("new-deck").addEventListener("click", getDeck);
 
-document.getElementById("draw").addEventListener("click", drawCards);
+newDeck.addEventListener("click", getDeck);
+newCards.addEventListener("click", drawCards);
